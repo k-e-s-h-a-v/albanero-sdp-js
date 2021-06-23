0a74810cce0e0ccefb98f10c3d17c8d33f5540a7
k-e-s-h-a-v <59414868+k-e-s-h-a-v@users.noreply.github.com>
2021-06-23 09:14:37 +0530

    Add files via upload

---
+++ fundamentals1/practice-problems/language-fundamentals/Expand-a-number.js
@@ -0,0 +1,10 @@
+function expand(num){
+    let numStr = num.toString().split('');
+    for(let i = 0 ; i < numStr.length; i++ ){
+        for(let y = numStr.length - i; y > 1; y--){
+           numStr[i] += '0'; 
+        }
+    }
+    return numStr.join('+')
+}
+console.log(expand(4356))
